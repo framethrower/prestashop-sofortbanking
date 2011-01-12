31594b4cc6744ef0d35449d135317940c945c3a9
Christin Gruber <c.gruber@touchdesign.de>
2011-01-12 12:53:10 +0000

    Update README, Update comments

--- sofortueberweisung.php
+++ sofortueberweisung.php
@@ -18,6 +18,18 @@
  *
  * Payment module directebanking
  *
+ * --
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@touchdesign.de so we can send you a copy immediately.
+ *
  */
 
 class Sofortueberweisung extends PaymentModule
