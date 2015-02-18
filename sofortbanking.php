273b19cc98ae9262314b2110cf1ae9c02d715ec2
Christin Gruber <c.gruber@touchdesign.de>
2015-02-18 12:19:03 +0100

    Add SOFORTBANKING_CONFIGURATION_OK key

--- sofortbanking.php
+++ sofortbanking.php
@@ -242,7 +242,7 @@ class Sofortbanking extends PaymentModule
 	 * @param array $params
 	 * @return array $result
 	 */
-	public function hookDisplayPaymentEU($params = null)
+	public function hookDisplayPaymentEU($params)
 	{
 		$lang = $this->isSupportedLang();
 		$result = array(
