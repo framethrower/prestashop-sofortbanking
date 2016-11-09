0331c59e967dc738d23efe74cf4e570f859933b9
Christin Gruber <c.gruber@touchdesign.de>
2016-11-09 18:02:59 +0100

    Set apiVersion to 2.0

--- lib/sofortlib/core/sofortLibAbstract.inc.php
+++ lib/sofortlib/core/sofortLibAbstract.inc.php
@@ -15,134 +15,134 @@ define('SOFORTLIB_VERSION', '2.1.2');
  * @license Released under the GNU LESSER GENERAL PUBLIC LICENSE (Version 3)
  * @license http://www.gnu.org/licenses/lgpl.html
  *
- * 
+ *
  * Base class for SOFORT XML-Api
- * 
+ *
  * This class implements basic http authentication and an xml-parser
  * for parsing response messages
  *
  * Requires libcurl and openssl
  */
 abstract class SofortLibAbstract {
-	
+
 	/**
 	 * API-URL
 	 */
 	CONST GATEWAY_URL = "https://api.sofort.com/api/xml";
-	
+
 	/**
 	 * Define if logging is en/disabled
 	 *
 	 * @var bool
 	 */
 	public $enableLogging = false;
-	
+
 	/**
 	 * Array for the errors that occurred
 	 *
 	 * @var array
 	 */
 	public $errors = array();
-	
+
 	/**
 	 * Array for the warnings that occurred
 	 *
 	 * @var array
 	 */
 	public $warnings = array();
-	
-	protected $_apiVersion = '1.0';
-	
+
+	protected $_apiVersion = '2.0';
+
 	/**
 	 * Api Key as provided in User Account on sofort.com
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $_apiKey = '';
-	
+
 	/**
 	 * Complete Config Key as provided in User Account on sofort.com
 	 *
 	 * @var string
 	 */
 	protected $_configKey = '';
-	
+
 	/**
 	 * Object for the Data Handler (usually XML-Data Handler)
 	 *
 	 * @var object
 	 */
 	protected $_DataHandler = null;
-	
+
 	/**
 	 * Object for the logger
 	 *
 	 * @var object
 	 */
 	protected $_Logger = null;
-	
+
 	/**
 	 * Array, that contains data and structure which will be send to the API (normally as XML)
 	 *
 	 * @var array
 	 */
 	protected $_parameters = array();
-	
+
 	/**
 	 * Contains the allowed products
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $_products = array('global', 'su');
-	
+
 	/**
 	 * Project ID from sofort.com
 	 *
 	 * @var string
 	 */
 	protected $_projectId = '';
-	
+
 	/**
 	 * Contains the request Data, that has been sent to the API
 	 *
 	 * @var array
 	 */
 	protected $_request;
-	
+
 	/**
 	 * Provides the parsed response.
 	 *
 	 * @var array
 	 */
 	protected $_response;
-	
+
 	/**
 	 * Defines the used part of the API
 	 *
 	 * @var string
 	 */
 	protected $_rootTag = '';
-	
+
 	/**
 	 * User ID from sofort.com
 	 *
 	 * @var string
 	 */
 	protected $_userId = '';
-	
+
 	/**
 	 * @var bool
 	 */
 	protected $_validateOnly = false;
-	
-	
+
+
 	public function __construct($configKey, $apiUrl = '') {
 		$this->setConfigKey($configKey);
-		
+
 		if ($apiUrl == '') {
 			$apiUrl = (getenv('sofortApiUrl') != '') ? getenv('sofortApiUrl') : self::GATEWAY_URL;
 		}
-		
+
 		$SofortLibHttp = SofortLibFactory::getHttpConnection($apiUrl);
 		$XmlDataHandler = SofortLibFactory::getDataHandler($configKey);
 		$this->setDataHandler($XmlDataHandler);
@@ -151,35 +151,35 @@ abstract class SofortLibAbstract {
 		$this->_DataHandler->setConnection($SofortLibHttp);
 		$this->enableLogging = (getenv('sofortDebug') == 'true') ? true : false;
 	}
-	
-	
+
+
 	/**
 	 * Getter for ConfigKey
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getConfigKey() {
 		return $this->_configKey;
 	}
-	
-	
+
+
 	/**
 	 * Preparing array for request
-	 * 
+	 *
 	 * @return array
 	 */
 	public function getData() {
 		if (in_array($this->_rootTag, array('multipay', 'paycode', 'billcode'))) {
 			$this->_parameters['project_id'] = $this->_projectId;
 		}
-		
+
 		$requestData[$this->_rootTag] = $this->_parameters;
 		$requestData = $this->_prepareRootTag($requestData);
-		
+
 		return $requestData;
 	}
-	
-	
+
+
 	/**
 	 * Getter for the DataHandler
 	 *
@@ -188,8 +188,8 @@ abstract class SofortLibAbstract {
 	public function getDataHandler() {
 		return $this->_DataHandler;
 	}
-	
-	
+
+
 	/**
 	 * Returns one error message
 	 *
@@ -202,25 +202,25 @@ abstract class SofortLibAbstract {
 		if (empty($message)) {
 			$message = $this->errors;
 		}
-		
+
 		if (!in_array($paymentMethod, $this->_products)) {
 			$paymentMethod = 'all';
 		}
-		
+
 		if (is_array($message) && !empty($message)) {
 			foreach ($message as $key => $error) {
 				$errorIsArrayAndNotEmpty = is_array($error) && !empty($error);
-				
+
 				if ($this->_getPaymentMethodAllPmGlobal($paymentMethod, $key) && $errorIsArrayAndNotEmpty) {
 					return 'Error: '.$error[0]['code'].':'.$error[0]['message'];
 				}
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Getter for errors
 	 *
@@ -232,30 +232,30 @@ abstract class SofortLibAbstract {
 		if (empty($message)) {
 			$message = $this->handleErrors($this->errors);
 		}
-		
+
 		if (!$this->isError($paymentMethod, $message)) {
 			return array();
 		}
-		
+
 		$supportedPaymentMethods = $this->_products;
-		
+
 		if (!in_array($paymentMethod, $supportedPaymentMethods)) {
 			$paymentMethod = 'all';
 		}
-		
+
 		$returnArray = array();
-		
+
 		//return global + selected payment method
 		foreach ($supportedPaymentMethods as $pm) {
 			if ($this->_getPaymentMethodAllPmGlobal($paymentMethod, $pm) && array_key_exists($pm, $message)) {
 				$returnArray = array_merge($returnArray, $message[$pm]);
 			}
 		}
-		
+
 		return $returnArray;
 	}
-	
-	
+
+
 	/**
 	 * Getter for LogHandler
 	 *
@@ -264,8 +264,8 @@ abstract class SofortLibAbstract {
 	public function getLogger() {
 		return $this->_Logger;
 	}
-	
-	
+
+
 	/**
 	 * Getter for parameter-array
 	 *
@@ -274,8 +274,8 @@ abstract class SofortLibAbstract {
 	public function getParameters() {
 		return $this->_parameters;
 	}
-	
-	
+
+
 	/**
 	 * Getter for the raw Request Data
 	 *
@@ -284,8 +284,8 @@ abstract class SofortLibAbstract {
 	public function getRawRequest() {
 		return $this->_DataHandler->getRawRequest();
 	}
-	
-	
+
+
 	/**
 	 * Getter for the raw Response Data
 	 *
@@ -294,8 +294,8 @@ abstract class SofortLibAbstract {
 	public function getRawResponse() {
 		return $this->_DataHandler->getRawResponse();
 	}
-	
-	
+
+
 	/**
 	 * Getter for the Request Data
 	 *
@@ -304,8 +304,8 @@ abstract class SofortLibAbstract {
 	public function getRequest() {
 		return $this->_request;
 	}
-	
-	
+
+
 	/**
 	 * Getter for the Response
 	 *
@@ -314,8 +314,8 @@ abstract class SofortLibAbstract {
 	public function getResponse() {
 		return $this->_response;
 	}
-	
-	
+
+
 	/**
 	 * Getter for warnings
 	 *
@@ -327,35 +327,35 @@ abstract class SofortLibAbstract {
 		if (empty($message)) {
 			$message = $this->warnings;
 		}
-		
+
 		$supportedPaymentMethods = $this->_products;
-		
+
 		if (!in_array($paymentMethod, $supportedPaymentMethods)) {
 			$paymentMethod = 'all';
 		}
-		
+
 		$returnArray = array();
-		
+
 		//return global + selected payment method
 		foreach ($supportedPaymentMethods as $pm) {
 			if (($paymentMethod == 'all' || $pm == 'global' || $paymentMethod == $pm) && array_key_exists($pm, $message)) {
 				$returnArray = array_merge($returnArray, $message[$pm]);
 			}
 		}
-		
+
 		return $returnArray;
 	}
-	
-	
+
+
 	/**
 	 * Alter error array and set error message and error code together as one
-	 * 
+	 *
 	 * @param array $errors
 	 * @return array (empty array if no error exist ELSE array with error-codes and error-messages)
 	 */
 	public function handleErrors($errors) {
 		$errorKeys = array_keys($errors);
-		
+
 		foreach ($errorKeys as $errorKey) {
 			foreach ($errors[$errorKey] as &$partialError) {
 				if (!empty($partialError['field']) && $partialError['field'] !== '') {
@@ -363,11 +363,11 @@ abstract class SofortLibAbstract {
 				}
 			};
 		}
-		
+
 		return $errors;
 	}
-	
-	
+
+
 	/**
 	 * checks (response)-array for error
 	 *
@@ -379,11 +379,11 @@ abstract class SofortLibAbstract {
 		if (empty($message)) {
 			$message = $this->errors;
 		}
-		
+
 		if (!in_array($paymentMethod, $this->_products)) {
 			$paymentMethod = 'all';
 		}
-		
+
 		if ($paymentMethod == 'all') {
 			return $this->_isErrorWarning($message);
 		} else {
@@ -392,11 +392,11 @@ abstract class SofortLibAbstract {
 				return $this->_getPaymentSpecificError($paymentMethod, $message);
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Checks (response)-array for warnings
 	 *
@@ -408,11 +408,11 @@ abstract class SofortLibAbstract {
 		if (empty($message)) {
 			$message = $this->warnings;
 		}
-		
+
 		if (!in_array($paymentMethod, $this->_products)) {
 			$paymentMethod = 'all';
 		}
-		
+
 		if ($paymentMethod == 'all') {
 			return $this->_isErrorWarning($message);
 		} else {
@@ -422,15 +422,15 @@ abstract class SofortLibAbstract {
 				}
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Log the given string into log.txt
 	 * use $this->enableLog(); to enable logging before!
-	 * 
+	 *
 	 * @param string $message
 	 * @return void
 	 */
@@ -439,8 +439,8 @@ abstract class SofortLibAbstract {
 			$this->_Logger->log($message, 'log');
 		}
 	}
-	
-	
+
+
 	/**
 	 * Log the given string into error_log.txt
 	 * use $this->enableLog(); to enable logging before!
@@ -453,8 +453,8 @@ abstract class SofortLibAbstract {
 			$this->_Logger->log($message, 'error');
 		}
 	}
-	
-	
+
+
 	/**
 	 * Log the given string into warning_log.txt
 	 * use $this->enableLog(); to enable logging before!
@@ -467,8 +467,8 @@ abstract class SofortLibAbstract {
 			$this->_Logger->log($message, 'warning');
 		}
 	}
-	
-	
+
+
 	/**
 	 * SendRequest sends Request (array) to the DataHandler and gets Response (array)
 	 *
@@ -487,8 +487,8 @@ abstract class SofortLibAbstract {
 		$this->_parse();
 		$this->_handleErrors();
 	}
-	
-	
+
+
 	/**
 	 * The customer will be redirected to this url if he uses the
 	 * abort link on the payment form, should redirect him back to
@@ -499,11 +499,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setAbortUrl($abortUrl) {
 		$this->_parameters['abort_url'] = $abortUrl;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Sets the version tag, appended to the root tag as attribute
 	 *
@@ -512,11 +512,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setApiVersion($apiVersion) {
 		$this->_apiVersion = $apiVersion;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Setter for ConfigKey and parsing ConfigKey into userId, projectId, apiKey
 	 *
@@ -526,11 +526,11 @@ abstract class SofortLibAbstract {
 	public function setConfigKey($configKey) {
 		$this->_configKey = $configKey;
 		list($this->_userId, $this->_projectId, $this->_apiKey) = explode(':', $configKey);
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Setter for Currency eg. EUR
 	 *
@@ -539,11 +539,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setCurrencyCode($currency) {
 		$this->_parameters['currency_code'] = $currency;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Setter for the DataHandler
 	 *
@@ -555,11 +555,11 @@ abstract class SofortLibAbstract {
 		$this->_DataHandler->setUserId($this->_userId);
 		$this->_DataHandler->setProjectId($this->_projectId);
 		$this->_DataHandler->setApiKey($this->_apiKey);
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Set Errors
 	 * later use getError(), getErrors() or isError() to retrieve them
@@ -572,20 +572,20 @@ abstract class SofortLibAbstract {
 	 */
 	public function setError($message, $pos = 'global', $errorCode = '-1', $field = '') {
 		$supportedErrorsPos = array('global', 'sr', 'su');
-		
+
 		if (!in_array($pos, $supportedErrorsPos)) {
 			$pos = 'global';
 		}
-		
+
 		if (!isset($this->errors[$pos])) {
 			$this->errors[$pos] = array();
 		}
-		
+
 		$error = array ('code' => $errorCode, 'message' => $message, 'field' => $field);
 		$this->errors[$pos][] = $error;
 	}
-	
-	
+
+
 	/**
 	 * Set logging disabled
 	 *
@@ -593,11 +593,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setLogDisabled() {
 		$this->enableLogging = false;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Set logging enable
 	 *
@@ -605,11 +605,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setLogEnabled() {
 		$this->enableLogging = true;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Setter for LogHandler
 	 *
@@ -618,11 +618,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setLogger(AbstractLoggerHandler $Logger) {
 		$this->_Logger = $Logger;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Sets the notification Email-address and it's attributes
 	 *
@@ -633,8 +633,8 @@ abstract class SofortLibAbstract {
 	public function setNotificationEmail($notificationAddress, $notifyOn = '') {
 		return $this->_setNotification($notificationAddress, 'email', $notifyOn);
 	}
-	
-	
+
+
 	/**
 	 * Sets the notification URL and it's attributes
 	 *
@@ -645,8 +645,8 @@ abstract class SofortLibAbstract {
 	public function setNotificationUrl($notificationAddress, $notifyOn = '') {
 		return $this->_setNotification($notificationAddress, 'url', $notifyOn);
 	}
-	
-	
+
+
 	/**
 	 * Setter for Parameter Array
 	 *
@@ -655,11 +655,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setParameters($parameters) {
 		$this->_parameters = $parameters;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Setter for redirecting the success link automatically
 	 *
@@ -668,11 +668,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setSuccessLinkRedirect($arg) {
 		$this->_parameters['success_link_redirect'] = $arg;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * The customer will be redirected to this url after a successful
 	 * transaction, this should be a page where a short confirmation is
@@ -685,11 +685,11 @@ abstract class SofortLibAbstract {
 	public function setSuccessUrl($successUrl, $redirect = true) {
 		$this->_parameters['success_url'] = $successUrl;
 		$this->setSuccessLinkRedirect($redirect);
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * If the customer takes too much time or if your timeout is set too short
 	 * he will be redirected to this page
@@ -699,11 +699,11 @@ abstract class SofortLibAbstract {
 	 */
 	public function setTimeoutUrl($timeoutUrl) {
 		$this->_parameters['timeout_url'] = $timeoutUrl;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Getter for error block
 	 *
@@ -714,11 +714,11 @@ abstract class SofortLibAbstract {
 		$newError['code'] = isset($error['code']['@data']) ? $error['code']['@data'] : '';
 		$newError['message'] = isset($error['message']['@data']) ? $error['message']['@data'] : '';
 		$newError['field'] = isset($error['field']['@data']) ? $error['field']['@data'] : '';
-		
+
 		return $newError;
 	}
-	
-	
+
+
 	/**
 	 * Handle Errors and Warnings occurred
 	 *
@@ -735,7 +735,7 @@ abstract class SofortLibAbstract {
 				}
 			}
 		}
-		
+
 		//handle warnings
 		if (isset($this->_response['new_transaction']['warnings']['warning'])) {
 			if (!isset($this->_response['new_transaction']['warnings']['warning'][0])) {
@@ -747,14 +747,14 @@ abstract class SofortLibAbstract {
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Parse data received or being sent
 	 */
 	protected function _parse() {}
-	
-	
+
+
 	/**
 	 * Set the type of notification and the address where it should be sent to
 	 * being sent to.
@@ -769,26 +769,26 @@ abstract class SofortLibAbstract {
 			$notifyOnArrayIn = explode(',', $notifyOn);
 			$notifyOnDefault = array('loss','pending','received','refunded', 'untraceable');
 			$notifyOnArray = array();
-			
+
 			if (is_array($notifyOnArrayIn)) foreach($notifyOnArrayIn as $notifyStatus) {
 				if (in_array($notifyStatus, $notifyOnDefault)) $notifyOnArray[] = $notifyStatus;
 			}
-			
+
 			$notifyOn = array('notify_on' => implode(',', $notifyOnArray));
 		}
-		
+
 		if (!$notifyOn) {
 			$notification = array('@data' => $notificationAddress);
 		} else {
 			$notification = array('@data' => $notificationAddress, '@attributes' => $notifyOn);
 		}
-		
+
 		$this->_parameters['notification_'.$notificationType.'s']['notification_'.$notificationType][] = $notification;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Helper function to compare given and supported Payment Method
 	 *
@@ -799,8 +799,8 @@ abstract class SofortLibAbstract {
 	private function _getPaymentMethodAllPmGlobal($paymentMethod, $pm) {
 		return $paymentMethod == 'all' || $pm == 'global' || $paymentMethod == $pm;
 	}
-	
-	
+
+
 	/**
 	 * checks (response)- for payment-specific error
 	 *
@@ -812,15 +812,15 @@ abstract class SofortLibAbstract {
 		$messagePaymentMethodSetNotEmpty = isset($message[$paymentMethod]) && !empty($message[$paymentMethod]);
 		$messageGlobalSetNotEmpty = isset($message['global']) && !empty($message['global']);
 		$messagePaymentMethodOrGlobalSetNotEmpty = $messagePaymentMethodSetNotEmpty || $messageGlobalSetNotEmpty;
-		
+
 		if ($messagePaymentMethodOrGlobalSetNotEmpty) {
 			return  true;
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Helper to iterate through an array of error or warning messages to find out whether
 	 * an error/warning occurred or not.
@@ -836,11 +836,11 @@ abstract class SofortLibAbstract {
 				}
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Prepare the root tag
 	 *
@@ -851,11 +851,11 @@ abstract class SofortLibAbstract {
 		if ($this->_apiVersion) {
 			$requestData[$this->_rootTag]['@attributes']['version'] = $this->_apiVersion;
 		}
-		
+
 		if ($this->_validateOnly) {
 			$requestData[$this->_rootTag]['@attributes']['validate_only'] = 'yes';
 		}
-		
+
 		return $requestData;
 	}
 }
